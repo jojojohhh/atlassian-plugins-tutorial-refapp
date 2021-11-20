382cf36a7188235dfd654e1c2426f5c4594bace0
jojojohhh <qwhtkdwp@gmail.com>
2021-11-20 23:06:05 +0900

    init

---
+++ src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
@@ -0,0 +1,23 @@
+package com.atlassian.plugins.tutorial.refapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class MyPluginServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(MyPluginServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("<html><body>Hello! You did it.</body></html>");
+    }
+}
