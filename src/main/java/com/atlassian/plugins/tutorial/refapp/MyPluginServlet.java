3ecf941e400f6c523c39ed640653c494174085ac
jojojohhh <qwhtkdwp@gmail.com>
2021-11-23 05:47:12 +0900

    Store and retrieve plugin data

--- src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
+++ src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
@@ -3,6 +3,8 @@ package com.atlassian.plugins.tutorial.refapp;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsDevService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.sal.api.user.UserProfile;
 import com.atlassian.templaterenderer.TemplateRenderer;
@@ -17,22 +19,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @ExportAsDevService
 @Component
 public class MyPluginServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(MyPluginServlet.class);
+    private static final String PLUGIN_STORAGE_KEY = "com.atlassian.plugins.tutorial.refapp.adminui";
 
     private final UserManager userManager;
     private final LoginUriProvider loginUriProvider;
     private final TemplateRenderer templateRenderer;
+    private final PluginSettingsFactory pluginSettingsFactory;
 
     @Autowired
-    public MyPluginServlet(@ComponentImport UserManager userManager,@ComponentImport LoginUriProvider loginUriProvider, @ComponentImport TemplateRenderer templateRenderer) {
+    public MyPluginServlet(@ComponentImport UserManager userManager,@ComponentImport LoginUriProvider loginUriProvider, @ComponentImport TemplateRenderer templateRenderer, @ComponentImport PluginSettingsFactory pluginSettingsFactory) {
         this.userManager = userManager;
         this.loginUriProvider = loginUriProvider;
         this.templateRenderer = templateRenderer;
+        this.pluginSettingsFactory = pluginSettingsFactory;
     }
 
     @Override
@@ -45,7 +52,32 @@ public class MyPluginServlet extends HttpServlet {
             redirectToLogin(req, res);
             return;
         }
+
+        Map<String, Object> context = new HashMap<>();
+
+        PluginSettings settings = pluginSettingsFactory.createGlobalSettings();
+        if (settings.get(PLUGIN_STORAGE_KEY + ".name") == null) {
+            String noName = "Enter a name here.";
+            settings.put(PLUGIN_STORAGE_KEY + ".name", noName);
+        }
+
+        if (settings.get(PLUGIN_STORAGE_KEY + ".age") == null) {
+            String noAge = "Enter a age here.";
+            settings.put(PLUGIN_STORAGE_KEY + ".age", noAge);
+        }
+
+        context.put("name", settings.get(PLUGIN_STORAGE_KEY + ".name"));
+        context.put("age", settings.get(PLUGIN_STORAGE_KEY + ".age"));
+        res.setContentType("text/html;charset=utf-8");
         templateRenderer.render("admin.vm", res.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        PluginSettings settings = pluginSettingsFactory.createGlobalSettings();
+        settings.put(PLUGIN_STORAGE_KEY + ".name", req.getParameter("name"));
+        settings.put(PLUGIN_STORAGE_KEY + ".age", req.getParameter("age"));
+        res.sendRedirect("test");
     }
 
     private void redirectToLogin(HttpServletRequest req, HttpServletResponse res) throws IOException {
